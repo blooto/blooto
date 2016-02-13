062b70c4fe0565bcc02931d67db6ab324a596d4a
Oleg Girko <ol@infoserver.lv>
2016-02-13 02:36:50 +0000

    Add iterator to BitBoard to iterate through squares it contains.
    De Bruijn Multiplication With separated LS1B by Kim Walisch is used
    for bitscan to extract bit number from bitmask, as described here:
    https://chessprogramming.wikispaces.com/Bitscan
    
    Tests for iteration added as well.

--- bitboard.hpp
+++ bitboard.hpp
@@ -17,6 +17,7 @@
 #define _BLOOTO_BITBOARD_HPP
 
 #include <cstdint>
+#include <iterator>
 
 #include "square.hpp"
 
@@ -83,6 +84,94 @@ namespace blooto {
             return *this;
         }
 
+        //! Iterator to traverse all squares in BitBoard
+        class iterator: std::iterator<std::forward_iterator_tag, Square>
+        {
+            data_type data_;
+
+            constexpr static const Square index64[64] = {
+                Square::A1, Square::H6, Square::B1, Square::A8,
+                Square::A7, Square::D4, Square::C1, Square::E8,
+                Square::B8, Square::B7, Square::B6, Square::F5,
+                Square::E4, Square::A3, Square::D1, Square::F8,
+                Square::G7, Square::C8, Square::D5, Square::E7,
+                Square::C7, Square::C6, Square::F3, Square::E6,
+                Square::G5, Square::A5, Square::F4, Square::H3,
+                Square::B3, Square::D2, Square::E1, Square::G8,
+                Square::G6, Square::H7, Square::C4, Square::D8,
+                Square::A6, Square::E5, Square::H2, Square::F7,
+                Square::C5, Square::D7, Square::E3, Square::D6,
+                Square::H4, Square::G3, Square::C2, Square::F6,
+                Square::B4, Square::H5, Square::G2, Square::B5,
+                Square::D3, Square::G4, Square::B2, Square::A4,
+                Square::F2, Square::C3, Square::A2, Square::E2,
+                Square::H1, Square::G1, Square::F1, Square::H8
+            };
+
+            constexpr static const data_type debruijn64 = 0x03f79d71b4cb0a89ULL;
+
+        public:
+
+            //! Construct empty iterator with no more squares left.
+            constexpr iterator(): data_(0ULL) {}
+
+            //! Construct iterator from BitBoard
+            constexpr explicit iterator(BitBoard bb): data_(bb.data()) {}
+
+            //! First square among remaining ones
+            //! @author Kim Walisch (2012)
+            //! @precondition bb != 0
+            //! @return index (0..63) of least significant one bit
+            constexpr Square operator*() const {
+                return index64[((data_ ^ (data_-1)) * debruijn64) >> 58];
+            }
+
+            //! Move iterator forward (remove first square)
+            //! @return reference to self
+            iterator &operator++() {
+                data_ &= data_ ^ (0 - data_);
+                return *this;
+            }
+
+            //! Move iterator forward (remove first square)
+            //! @return reference to self
+            //! This is post-increment operator,
+            //! moving iterator forward, but returning
+            //! old iterator's value.
+            iterator operator++(int) {
+                iterator tmp = *this;
+                data_ &= data_ ^ (0 - data_);
+                return tmp;
+            }
+
+            //! Compare iterator with another one
+            //! @param rhs another iterator
+            //! @return true is iterators are equal
+            constexpr bool operator==(iterator rhs) const {
+                return data_ == rhs.data_;
+            }
+
+            //! Compare iterator with another one
+            //! @param rhs another iterator
+            //! @return true is iterators are not equal
+            constexpr bool operator!=(iterator rhs) const {
+                return data_ != rhs.data_;
+            }
+
+        };
+
+        //! Make iterator pointing to the beginning of squares
+        //! @return new iterator
+        constexpr iterator begin() const {
+            return iterator(*this);
+        }
+
+        //! Make iterator pointing after the end of squares
+        //! @return new iterator
+        constexpr iterator end() const {
+            return iterator();
+        }
+
     private:
         data_type data_;
 
@@ -145,6 +234,7 @@ namespace blooto {
         return ~BitBoard(sq);
     }
 
+    constexpr const Square BitBoard::iterator::index64[];
 }
 
 #endif
--- tests/test_bitboard.cpp
+++ tests/test_bitboard.cpp
@@ -1,3 +1,5 @@
+#include <initializer_list>
+
 #include "bitboard.hpp"
 
 #define BOOST_TEST_DYN_LINK
@@ -8,14 +10,26 @@ BOOST_AUTO_TEST_CASE(test_bitboard) {
     blooto::BitBoard empty_bb;
     BOOST_CHECK(!empty_bb[blooto::Square::A1]);
     BOOST_CHECK(empty_bb.data() == 0);
+    std::initializer_list<blooto::Square> empty_il{};
+    BOOST_CHECK_EQUAL_COLLECTIONS(empty_bb.begin(), empty_bb.end(),
+                                  empty_il.begin(), empty_il.end());
     blooto::BitBoard all_bb(~empty_bb);
     BOOST_CHECK(all_bb[blooto::Square::A1]);
     blooto::BitBoard d4_bb(blooto::Square::D4);
     BOOST_CHECK(!d4_bb[blooto::Square::A1]);
     BOOST_CHECK(d4_bb[blooto::Square::D4]);
+    std::initializer_list<blooto::Square> d4_il{blooto::Square::D4};
+    BOOST_CHECK_EQUAL_COLLECTIONS(d4_bb.begin(), d4_bb.end(),
+                                  d4_il.begin(), d4_il.end());
     blooto::BitBoard b2_f6_bb(blooto::Square::B2 | blooto::Square::F6);
     BOOST_CHECK(!b2_f6_bb[blooto::Square::A1]);
     BOOST_CHECK(!b2_f6_bb[blooto::Square::D4]);
     BOOST_CHECK(b2_f6_bb[blooto::Square::B2]);
     BOOST_CHECK(b2_f6_bb[blooto::Square::F6]);
+    std::initializer_list<blooto::Square> b2_f6_il{
+        blooto::Square::B2,
+        blooto::Square::F6
+    };
+    BOOST_CHECK_EQUAL_COLLECTIONS(b2_f6_bb.begin(), b2_f6_bb.end(),
+                                  b2_f6_il.begin(), b2_f6_il.end());
 }

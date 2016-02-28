73c8bbf93853b469e6c29275468e16dee8e6167d
Oleg Girko <ol@infoserver.lv>
2016-02-28 23:05:20 +0000

    Add PieceColour and MoveColour classes for piece and move colour.
    These classes are discriminated unions (using boost::variant) of
    ColourNeutral, ColourWhite and ColourBlack tag types for PieceColour, and
    ColourWhite and ColourBlack tag types for MoveColour.
    
    PieceColour and MoveColour classes have methods to check friendliness
    of other colours.
    Output operators are provided as well for these classes and tag types.
    
    Unit tests added as well.

---
+++ blooto/colour.hpp
@@ -0,0 +1,267 @@
+// This file is part of Blooto.
+//
+// Blooto is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Blooto is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _BLOOTO_COOUR_HPP
+#define _BLOOTO_COOUR_HPP
+
+#include <boost/variant.hpp>
+
+namespace blooto {
+
+    //! Tag for neutral pieces.
+    struct ColourNeutral {};
+
+    //! Tag for white pieces.
+    struct ColourWhite {};
+
+    //! Tag for black pieces.
+    struct ColourBlack {};
+
+    //! Output neutral colour name to output stream
+    //! @param out output stream
+    //! @return reference to output stream for operator chaining
+    template<typename CharT, typename CharTraits>
+    std::basic_ostream<CharT, CharTraits> &
+    operator<<(std::basic_ostream<CharT, CharTraits> &out, ColourNeutral)
+    {
+        static const CharT name[] = {'n', 'e', 'u', 't', 'r', 'a', 'l', '\0'};
+        return out << name;
+    }
+
+    //! Output white colour name to output stream
+    //! @param out output stream
+    //! @return reference to output stream for operator chaining
+    template<typename CharT, typename CharTraits>
+    std::basic_ostream<CharT, CharTraits> &
+    operator<<(std::basic_ostream<CharT, CharTraits> &out, ColourWhite)
+    {
+        static const CharT name[] = {'w', 'h', 'i', 't', 'e', '\0'};
+        return out << name;
+    }
+
+    //! Output black colour name to output stream
+    //! @param out output stream
+    //! @return reference to output stream for operator chaining
+    template<typename CharT, typename CharTraits>
+    std::basic_ostream<CharT, CharTraits> &
+    operator<<(std::basic_ostream<CharT, CharTraits> &out, ColourBlack)
+    {
+        static const CharT name[] = {'b', 'l', 'a', 'c', 'k', '\0'};
+        return out << name;
+    }
+
+    //! @cond detail
+    namespace detail {
+
+        struct friendly_or_neutral_visitor: boost::static_visitor<bool> {
+            bool operator()(ColourWhite, ColourBlack) const {return false;}
+            bool operator()(ColourBlack, ColourWhite) const {return false;}
+            template <typename C1, typename C2>
+            bool operator()(C1, C2) const {return true;}
+        };
+
+        struct friendly_only_visitor: boost::static_visitor<bool> {
+            bool operator()(ColourWhite, ColourWhite) const {return true;}
+            bool operator()(ColourBlack, ColourBlack) const {return true;}
+            template <typename C1, typename C2>
+            bool operator()(C1, C2) const {return false;}
+        };
+
+        struct equal_type_visitor: boost::static_visitor<bool> {
+            template <typename T, typename U>
+            bool operator()(T, U) const {return false;}
+            template <typename T> bool operator()(T, T) const {return true;}
+        };
+
+        template <typename CharT, typename CharTraits>
+        class output_visitor:
+        public boost::static_visitor<std::basic_ostream<CharT, CharTraits> &>
+        {
+            using ostream_type = std::basic_ostream<CharT, CharTraits>;
+            ostream_type &out_;
+        public:
+            constexpr output_visitor(ostream_type &out): out_(out) {}
+            template <typename T> ostream_type &operator()(const T &obj) const
+            {
+                return out_ << obj;
+            }
+        };
+    }
+    //! @endcond
+
+    //! Piece colour
+    class PieceColour: public boost::variant<ColourNeutral,
+                                             ColourWhite,
+                                             ColourBlack>
+    {
+        using base_type = boost::variant<ColourNeutral,
+                                         ColourWhite,
+                                         ColourBlack>;
+
+    public:
+
+        //! Construct colour for neutral piece
+        //! @param arg colour tag
+        PieceColour(ColourNeutral arg): base_type{arg} {}
+
+        //! Construct colour for white piece
+        //! @param arg colour tag
+        PieceColour(ColourWhite arg): base_type{arg} {}
+
+        //! Construct colour for black piece
+        //! @param arg colour tag
+        PieceColour(ColourBlack arg): base_type{arg} {}
+
+        //! Default copy constructor
+        //! @param other object to construct from
+        PieceColour(const PieceColour &other) = default;
+
+        //! Default move constructor
+        //! @param other object to construct from
+        PieceColour(PieceColour &&other) = default;
+
+        //! Check for other piece's friendliness.
+        //! @param c colour of other piece
+        //! @return true if other piece is friendly or neutral, false otherwise
+        bool friendly_or_neutral(PieceColour c) const {
+            return boost::apply_visitor(detail::friendly_or_neutral_visitor(),
+                                        *this, c);
+        }
+
+        //! Compare for equality.
+        //! @param rhs other colour
+        //! @return true if both colours are the same
+        bool operator==(PieceColour rhs) const {
+            return boost::apply_visitor(detail::equal_type_visitor(),
+                                        *this, rhs);
+        }
+
+        //! Compare for inequality.
+        //! @param rhs other colour
+        //! @return true if both colours are not the same
+        bool operator!=(PieceColour rhs) const {
+            return !boost::apply_visitor(detail::equal_type_visitor(),
+                                         *this, rhs);
+        }
+
+    };
+
+    //! Output piece colour to output stream
+    //! @param out output stream
+    //! @param pc piece colour to output
+    //! @return reference to output stream for operator chaining
+    template<typename CharT, typename CharTraits>
+    std::basic_ostream<CharT, CharTraits> &
+    operator<<(std::basic_ostream<CharT, CharTraits> &out, PieceColour pc)
+    {
+        using visitor = detail::output_visitor<CharT, CharTraits>;
+        return boost::apply_visitor(visitor(out), pc);
+    }
+
+    //! Move colour
+    class MoveColour: public boost::variant<ColourWhite, ColourBlack> {
+        using base_type = boost::variant<ColourWhite, ColourBlack>;
+
+    public:
+
+        //! Construct white move colour
+        //! @param arg colour tag
+        MoveColour(ColourWhite arg): base_type{arg} {}
+
+        //! Construct black move colour
+        //! @param arg colour tag
+        MoveColour(ColourBlack arg): base_type{arg} {}
+
+        //! Default copy constructor
+        //! @param other object to construct from
+        MoveColour(const MoveColour &other) = default;
+
+        //! Default move constructor
+        //! @param other object to construct from
+        MoveColour(MoveColour &&other) = default;
+
+        //! Opposite move colour.
+        //! @return colour opposite to this one
+        MoveColour opposite() const {
+            struct visitor: boost::static_visitor<MoveColour> {
+                MoveColour operator()(ColourWhite) const {return ColourBlack();}
+                MoveColour operator()(ColourBlack) const {return ColourWhite();}
+            };
+            return boost::apply_visitor(visitor(), *this);
+        }
+
+        //! Check if a piece can be moved during this move.
+        //! @param c colour of the piece
+        //! @return true if the piece can move, false otherwise
+        bool can_move(PieceColour c) const {
+            return boost::apply_visitor(detail::friendly_or_neutral_visitor(),
+                                        *this, c);
+        }
+
+        //! Check is a piece is friendly (can't be attacked).
+        //! @param c colour of the piece
+        //! @return true if other piece can't be attacked, false otherwise
+        bool friendly(PieceColour c) const {
+            return boost::apply_visitor(detail::friendly_only_visitor(),
+                                        *this, c);
+        }
+
+        //! Convert move colour to piece colour
+        //! @return piece colour corresponding to move colour
+        PieceColour to_piece_colour() const {
+            struct visitor: boost::static_visitor<PieceColour> {
+                PieceColour operator()(ColourWhite) const {
+                    return ColourWhite();
+                }
+                PieceColour operator()(ColourBlack) const {
+                    return ColourBlack();
+                }
+            };
+            return boost::apply_visitor(visitor(), *this);
+        }
+
+        //! Compare for equality.
+        //! @param rhs other colour
+        //! @return true if both colours are the same
+        bool operator==(MoveColour rhs) const {
+            return boost::apply_visitor(detail::equal_type_visitor(),
+                                        *this, rhs);
+        }
+
+        //! Compare for inequality.
+        //! @param rhs other colour
+        //! @return true if both colours are not the same
+        bool operator!=(MoveColour rhs) const {
+            return !boost::apply_visitor(detail::equal_type_visitor(),
+                                         *this, rhs);
+        }
+
+    };
+
+    //! Output move colour to output stream
+    //! @param out output stream
+    //! @param mc move colour to output
+    //! @return reference to output stream for operator chaining
+    template<typename CharT, typename CharTraits>
+    std::basic_ostream<CharT, CharTraits> &
+    operator<<(std::basic_ostream<CharT, CharTraits> &out, MoveColour mc)
+    {
+        using visitor = detail::output_visitor<CharT, CharTraits>;
+        return boost::apply_visitor(visitor(out), mc);
+    }
+
+}
+
+#endif
---
+++ tests/test_colour.cpp
@@ -0,0 +1,79 @@
+// This file is part of Blooto.
+//
+// Blooto is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Blooto is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <string>
+#include <boost/lexical_cast.hpp>
+
+#include <blooto/colour.hpp>
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE test_colour
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(test_piececolour) {
+    using namespace blooto;
+    PieceColour pcn{ColourNeutral()};
+    BOOST_CHECK_EQUAL(pcn, ColourNeutral());
+    BOOST_CHECK_EQUAL(boost::lexical_cast<std::string>(pcn), "neutral");
+    PieceColour pcw{ColourWhite()};
+    BOOST_CHECK_EQUAL(pcw, ColourWhite());
+    BOOST_CHECK_EQUAL(boost::lexical_cast<std::string>(pcw), "white");
+    PieceColour pcb{ColourBlack()};
+    BOOST_CHECK_EQUAL(pcb, ColourBlack());
+    BOOST_CHECK_EQUAL(boost::lexical_cast<std::string>(pcb), "black");
+    // Neutral is friendly_or_neutral for everything.
+    BOOST_CHECK(pcn.friendly_or_neutral(pcn));
+    BOOST_CHECK(pcn.friendly_or_neutral(pcb));
+    BOOST_CHECK(pcn.friendly_or_neutral(pcw));
+    // White is friendly_or_neutral for everything but black.
+    BOOST_CHECK(pcw.friendly_or_neutral(pcn));
+    BOOST_CHECK(pcw.friendly_or_neutral(pcw));
+    BOOST_CHECK(!pcw.friendly_or_neutral(pcb));
+    // Black is friendly_or_neutral for everything but white.
+    BOOST_CHECK(pcb.friendly_or_neutral(pcn));
+    BOOST_CHECK(!pcb.friendly_or_neutral(pcw));
+    BOOST_CHECK(pcb.friendly_or_neutral(pcb));
+}
+
+BOOST_AUTO_TEST_CASE(test_movecolour) {
+    using namespace blooto;
+    MoveColour mcw{ColourWhite()};
+    BOOST_CHECK_EQUAL(mcw, ColourWhite());
+    BOOST_CHECK_EQUAL(boost::lexical_cast<std::string>(mcw), "white");
+    MoveColour mcb{ColourBlack()};
+    BOOST_CHECK_EQUAL(mcb, ColourBlack());
+    BOOST_CHECK_EQUAL(boost::lexical_cast<std::string>(mcb), "black");
+    PieceColour pcn{ColourNeutral()};
+    PieceColour pcw{ColourWhite()};
+    PieceColour pcb{ColourBlack()};
+    BOOST_CHECK_EQUAL(mcw.to_piece_colour(), pcw);
+    BOOST_CHECK_EQUAL(mcb.to_piece_colour(), pcb);
+    // White is friendly to white only.
+    BOOST_CHECK(!mcw.friendly(pcn));
+    BOOST_CHECK(mcw.friendly(pcw));
+    BOOST_CHECK(!mcw.friendly(pcb));
+    // Black is friendly to black only.
+    BOOST_CHECK(!mcb.friendly(pcn));
+    BOOST_CHECK(!mcb.friendly(pcw));
+    BOOST_CHECK(mcb.friendly(pcb));
+    // White and neutral pieces can move during white move.
+    BOOST_CHECK(mcw.can_move(pcn));
+    BOOST_CHECK(mcw.can_move(pcw));
+    BOOST_CHECK(!mcw.can_move(pcb));
+    // Black and neutral pieces can move during black move.
+    BOOST_CHECK(mcb.can_move(pcn));
+    BOOST_CHECK(!mcb.can_move(pcw));
+    BOOST_CHECK(mcb.can_move(pcb));
+}
